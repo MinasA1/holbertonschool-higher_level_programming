3790e83249980a674e6d0902bd9ba86ba624d12f
MinasA1 <minasanton@gmail.com>
2017-08-03 23:55:20 +0000

    changed print format

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     div = __import__('calculator_1').div
     a = 10
     b = 5
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    print("{:d} {:s} {:d} = {:d}".format(a, "+", b, add(a, b)))
+    print("{:d} {:s} {:d} = {:d}".format(a, "-", b, sub(a, b)))
+    print("{:d} {:s} {:d} = {:d}".format(a, "*", b, mul(a, b)))
+    print("{:d} {:s} {:d} = {:d}".format(a, "/", b, div(a, b)))
