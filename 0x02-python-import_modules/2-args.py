df832ab0001174e9b4ec7907ddda3c6a7cbf59f6
MinasA1 <minasanton@gmail.com>
2017-08-04 00:06:21 +0000

    fixed print output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,9 @@
 if __name__ == "__main__":
     import sys
     l = len(sys.argv) - 1
-    print("{:d} {:s}:".format(l, "arguments" if l > 1 else "argument"))
-    for i in range(1, len(sys.argv)):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+    if l != 0:
+        print("{:d} {:s}:".format(l, "arguments" if l > 1 else "argument"))
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
+    else:
+        print("{:d} {:s}".format(l, "argument."))
