6427298ccf783166ece6f6ddc72d2bbff513b98e
MinasA1 <minasanton@gmail.com>
2017-08-09 01:46:09 +0000

    pep8 style corrected

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,8 +3,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
     c = [0] * 2
     for i, v in enumerate(tuple_a):
         c[i] += v
-        if i == 1: break
+        if i == 1:
+            break
     for i, v in enumerate(tuple_b):
         c[i] += v
-        if i == 1: break
+        if i == 1:
+            break
     return tuple(c)
