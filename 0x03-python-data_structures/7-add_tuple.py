f204c890f7cc7c58c93e404256a7a05653d97fe6
MinasA1 <minasanton@gmail.com>
2017-08-09 01:44:44 +0000

    revised

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,25 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    len_a = len(tuple_a)
-    len_b = len(tuple_b)
-    tlist = [0] * 2
-    if len_a == 2:
-        lim_len_a = 2
-    elif len_a > 2:
-        lim_len_a = 2
-    elif len_a < 2:
-        lim_len_a = len_a
-    if len_a != 0:
-        for i in range(0, lim_len_a):
-            tlist[i] = tuple_a[i]
-    if len_b == 2:
-        lim_len_b = 2
-    elif len_b > 2:
-        lim_len_b = 2
-    elif len_b < 2:
-        lim_len_b = len_b
-    if len_b != 0:
-        for i in range(0, lim_len_b):
-            tlist[i] += tuple_b[i]
-    tuple(tlist)
-    return(tlist)
+    c = [0] * 2
+    for i, v in enumerate(tuple_a):
+        c[i] += v
+        if i == 1: break
+    for i, v in enumerate(tuple_b):
+        c[i] += v
+        if i == 1: break
+    return tuple(c)
