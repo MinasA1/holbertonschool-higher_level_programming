288630170f9ec6765be992d8a8a0baef355533f5
MinasA1 <minasanton@gmail.com>
2017-08-14 06:43:34 +0000

    README

--- 0x04-python-more_date_structures/0-square_matrix_simple.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for i, row in enumerate(matrix):
-        new_matrix.append([])
-        for j, item in enumerate(row):
-            new_matrix[i].append(item ** 2)
-    return(new_matrix)
--- 0x04-python-more_date_structures/2-uniq_add.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def uniq_add(my_list=[]):
-    new_list = list(set(my_list))
-    s = sum(new_list)
-    return(s)
