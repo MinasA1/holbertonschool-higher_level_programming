6b51286582d2ef927dc62a8c3cbbbd5704ff5dc1
Minas <minasanton@gmail.com>
2017-11-22 06:18:27 +0000

    revert changes

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,14 +4,13 @@
 
 def find_peak(list_of_integers):
     """finds peak number"""
-    if list_of_integers:
-        def f_peak(low, high):
-            """helper function to achieve real log(n)"""
-            l = (low + high)//2
-            if l > 0 and list_of_integers[l] < list_of_integers[l-1]:
-                return f_peak(low, m)
-            elif l < (low + high) and list_of_integers[l] < list_of_integers[l+1]:
-                return f_peak(l, high)
-            else:
-                return list_of_integers[l]
-        return f_peak(0, len(list_of_integers))
+    if not list_of_integers:
+        return
+    s = len(list_of_integers)
+    l = s//2
+    if l > 0 and list_of_integers[l] < list_of_integers[l-1]:
+        return find_peak(list_of_integers[:l])
+    elif l < s-1 and list_of_integers[l] < list_of_integers[l+1]:
+        return find_peak(list_of_integers[l:])
+    else:
+        return list_of_integers[l]
---
+++ 0x10-python-network_0/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Test function find_peak """
+find_peak = __import__('6-peak').find_peak
+
+print(find_peak([1, 2, 4, 6, 3]))
+print(find_peak([4, 2, 1, 2, 3, 1]))
+print(find_peak([5,3,7,8,4]))
+print(find_peak([100,0,0,0, 40, 34, 45, 435435435, 34534534534]))
