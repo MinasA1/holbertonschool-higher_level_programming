73359d4a256766b81c382f62c3bf07fcca6d57d1
Minas <minasanton@gmail.com>
2017-11-28 03:22:35 +0000

    added try except in case key not valid

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,9 +4,12 @@ from requests import get
 from sys import argv
 
 
-if len(argv) == 1:
+if len(argv) < 2:
     print("Usage ./5-hbtn_header <URL>")
 else:
     url = argv[1]
     html = get(url)
-    print(html.headers['X-Request-Id'])
+    try:
+        print(html.headers['X-Request-Id'])
+    except KeyError:
+        pass
