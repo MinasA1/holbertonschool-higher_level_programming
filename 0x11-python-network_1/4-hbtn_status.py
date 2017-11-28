f7a0467709fe3e612441d529cfe86ed16256c624
Minas <minasanton@gmail.com>
2017-11-28 03:11:08 +0000

    tab fix for 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ from requests import get
 
 html = get('https://intranet.hbtn.io/status').content.decode()
 print("Body response:")
-print("    - type: {}".format(type(html)))
-print("    - content: {}".format(html))
+print("\t- type: {}".format(type(html)))
+print("\t- content: {}".format(html))
