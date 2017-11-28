643cff664e8aa10693b49154d4aede1847261bcb
Minas <minasanton@gmail.com>
2017-11-28 03:09:09 +0000

    fix fo 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ import urllib.request as http
 with http.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
     print("Body response:")
-    print("    - type: {}".format(type(html)))
-    print("    - content: {}".format(html))
-    print("    - utf8 content: {}".format(html.decode()))
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode()))
