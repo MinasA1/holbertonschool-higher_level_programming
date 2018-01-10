4016188b195c7c90fdf88aaccfb65422d0f0bf44
Minas <minasanton@gmail.com>
2018-01-10 22:31:25 +0000

    strict check for url

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,6 +3,12 @@ if (!(process.argv.length === 3)) {
   console.log('Usage: ./starwars_count.js <API>');
   process.exit();
 }
+if (!(process.argv[2] === 'http://swapi.co/api/films/' ||
+      process.argv[2] === 'https://swapi.co/api/films/' ||
+      process.argv[2] === 'https://swapi.co/api/films' ||
+      process.argv[2] === 'http://swapi.co/api/films')) {
+  process.exit();
+}
 const request = require('request');
 let name = 'https://swapi.co/api/people/18/';
 request(process.argv[2], function (error, response, body) {
