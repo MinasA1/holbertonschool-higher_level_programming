6e3a607795fc15df3c37f56d9a54997d99fc320f
Minas <minasanton@gmail.com>
2018-01-10 07:40:53 +0000

    changed var to let

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
-var newList = list.map(function (cV, i) {
+let newList = list.map(function (cV, i) {
   return cV * i;
 });
 console.log(list);
