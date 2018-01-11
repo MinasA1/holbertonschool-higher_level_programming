42eb288d72f49be904f072b55d462b603863ec73
Minas <minasanton@gmail.com>
2018-01-11 04:02:05 +0000

    fix 101 with promises

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,20 +5,27 @@ if (!(process.argv.length === 3)) {
 }
 const request = require('request');
 let url = 'https://swapi.co/api/films/' + process.argv[2];
+function syncReq (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, response, body) {
+      if (error) reject(error);
+      resolve(JSON.parse(body).name);
+    });
+  });
+}
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
     process.exit();
   }
   let js = JSON.parse(body);
+  let resolves = []
   for (let el in js['characters']) {
-    request(js['characters'][el], function (error, response, body) {
-      if (error) {
-        console.log(error);
-      } else {
-        let js = JSON.parse(body);
-        console.log(js['name']);
-      }
-    });
+    resolves.push(syncReq(js['characters'][el]));
   }
+  Promise.all(resolves).then(function (names) {
+    for (let name of names) {
+      console.log(name);
+    }
+  });
 });
